859a54d7d8284d1a9bac6892cc33c8721884e6e7
anushac14 <113647676+anushac14@users.noreply.github.com>
2024-07-22 03:56:55 -0400

    Initial Commit

---
+++ CollectionsHub/CollectionsHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionsHubApp.swift
+//  CollectionsHub
+//
+//  Created by Anusha Chinthamaduka on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CollectionsHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
