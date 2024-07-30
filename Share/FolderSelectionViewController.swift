928d009d959da311d65b5e4a8698522585e37b14
anushac14 <113647676+anushac14@users.noreply.github.com>
2024-07-30 05:29:09 -0400

    Got link sharing functionality to work for safari

---
+++ Share/FolderSelectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FolderSelectionViewController.swift
+//  CollectionsHub
+//
+//  Created by Anusha Chinthamaduka on 7/30/24.
+//
+
+import SwiftUI
+
+struct FolderSelectionViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FolderSelectionViewController()
+}
